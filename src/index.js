a5058569b87e2e20e62d134b945eb044517561bb
kiran kumar reddy <kirankumarrrreddy@gmail.com>
2019-08-14 18:12:36 +0530

    check1

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.css";
 import Newapp from "./newApp";
-import LifeCycle from "./LifeCycle";
+import LifeCycle from "./LifeCycle"; 
+//
 ReactDOM.render(<App />, document.getElementById("root"));
